af8afd04fea9bbedbd636178f2a229c1eb219a72
MickaHais <m_hais@hetic.eu>
2024-05-04 17:21:54 +0200

    Ajout page de connexion

---
+++ src/app/pages/pages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    );
+};
+
+export default Home;
