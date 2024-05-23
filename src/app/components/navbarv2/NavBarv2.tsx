1864a59ad5186a15b7ffe39879ef29d326758695
Alexis G <alexis.gontier03@gmail.com>
2024-05-23 06:40:14 +0200

    création

---
+++ src/app/components/navbarv2/NavBarv2.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./StyleNav.module.css"
+import DekupleLogo from "@/src/public/image/Dekuple_logo.png"
+import Link from "next/link"
+
+export default function NavBarv2() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
