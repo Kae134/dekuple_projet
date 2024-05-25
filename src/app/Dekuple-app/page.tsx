d1ca3a96a17cbf2a5a92149972ae4fdbfdbd33f2
Alexis G <alexis.gontier03@gmail.com>
2024-05-25 23:22:56 +0200

    en +

--- src/app/Dekuple-app/page.tsx
+++ src/app/Dekuple-app/page.tsx
@@ -1,7 +1,15 @@
 import { redirect } from 'next/navigation'
+import NavBarv2 from '../components/navbarv2/NavBarv2';
+import Notifications from '../components/notifications/Notifications'
+import CardGraph from '../components/cardGraph/CardGraph'
 
 export default async function DekupleApp() {
     return (
-        redirect('/Dekuple-app/accueil')
+        // redirect('/Dekuple-app/accueil')
+        <div>
+            <CardGraph color="#FF3125" />
+            <CardGraph color="#4CAF50" />
+            <CardGraph color="#2196F3" />
+        </div>
     )
 }
