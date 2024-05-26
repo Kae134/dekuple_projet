69f03d2c01368949c5c1496e833bfb96af2be809
Alexis G <alexis.gontier03@gmail.com>
2024-05-26 15:12:24 +0200

    modif drag and drop

--- src/app/components/dragAndDrop/Drag.tsx
+++ src/app/components/dragAndDrop/Drag.tsx
@@ -4,14 +4,15 @@ import styles from "./Drag.module.css";
 
 export default function Drag() {
     return (
-        <div className="card">
+        <div className={styles.card}>
             <FileUpload
+                className={styles.fileUpload}
                 name="demo[]"
                 url={'/api/upload'}
                 multiple accept="image/*"
                 maxFileSize={1000000}
                 emptyTemplate={
-                <p className="m-0">Ajoutez les données.</p>
+                <p className={styles.p}>Ajoutez les données :</p>
                 }
             />
         </div>
